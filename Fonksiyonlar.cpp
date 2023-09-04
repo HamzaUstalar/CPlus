7ec0bef40a54fb5376abfec5a88f66b3a2615833
hamza ustalar <hamzaustalar115@gmail.com>
2023-09-04 21:52:12 +0300

    Cplus
    Learning C++ From Start To Finish

---
+++ Fonksiyonlar.cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+#include <string>
+
+// FONKSİYONLAR
+// FUNCTİONS
+
+/*
+
+// void değer döndürmez.
+// void returns no value.
+// value=değer.
+
+void send () { 
+    std::cout << "Hello Guys\n";
+}
+int main(){
+    send ();
+}
+
+*/
+
+
+/*
+void hello (char msg,char xx) { // İki tane char tanımladım. Charı tek tanımladığım için tek bir harf yazabiliriz.
+    std::cout << msg << " " << xx;
+}
+int main () {
+    hello ('D','f'); // Burada da  tek kelimeleri çağırdık.
+}
+*/
+
+
+/*
+void message () {
+    std::cout << "Merhabalar" << std::endl;
+}
+int collec (int number1, int number2) {
+    int topla;
+    topla = number1 + number2;
+    message (); // Fonksiyondan fonksiyonu çağırabiliriz. Fakat fonksiyon içinde fonksiyon çalıştıramayız.
+    return topla;
+}
+int main () {
+    std::cout << collec (10, 20);
+}
+*/
+
+
+/*
+// İleriye Dönük Bildirim...
+// Bildirim Ve Tanımlama
+int topla (int number1, int number2); // Bildirim
+int main () {
+    std::cout << topla (100, 240) << std::endl;
+
+}
+int topla (int number1, int number2) { // Tanımlama
+    int topla = number1 + number2;
+    return topla;
+}
+*/
+
+
+/*
+int collect (int number1, int number2) {
+    std::cout << "int==>";
+    return number1 + number2;
+}
+double collect (double number1, double number2) {
+    std::cout << "double==>";
+    return number1 + number2;
+}
+int main () {
+    std::cout << 5421212121214L << std::endl;
+    std::cout << collect (10, 60) << std::endl; // int==>70
+    std::cout << collect (10.02, 30.12) << std::endl; // double==>40.14
+}
+// collect(int,double) veya tam tersi kullanılamaz...
+*/
+
+
+/*
+void print (std::string hello = "Merhabalar",int a = 10,bool bb= true ) {
+    std::cout << hello << " " << a << std::endl;
+}
+int main () {
+    print ("naber",30,true); // naber 30
+    print ();           // hello 10
+}
+// Stringi tanımlarken std::string olarak tanımlamaya özen göster.
+// When defining string,it is defined as std::string
+// defining=tanımlayan defined=tanımlanmış as=olarak 
+*/
+//int topla (int a, int b) {
+//    return a + b;
+//}
+//int islemyap (int x, int y, int(*callback)(int, int)) {
+//    return callback (x, y);
+//}
+//int main () {
+//    int sonuc = islemyap (10, 30, topla);
+//    std::cout << "Sonuc==> " << sonuc << std::endl;
+//    
+//    return 0;
+//}
+//
+//int main () {
+//    int a = 50;
+//    std::cout << *&a;
+//}
+
+// INLINE FUNCTION
+
+//inline int topla (int a, int b) { // Normal fonksiyondan daha az işlevli olduğu için basit işlemlerde kullanılır.
+//    return a + b;
+//}
+//int main () {
+//    std::cout << topla (300 , 20);
+//}
+//
+
+
+
---
+++ Temel-Bilgiler-Stream.cpp
@@ -0,0 +1,150 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <wchar.h>
+
+
+
+// Learning English And Basic Information
+
+
+
+// using namespace std;
+// Büyük projelerde sorun yaratır. O yüzden std kullanımına alışkanlık haline getiriyorum...
+// Creates problems in large projects
+// Creates=Yaratır large=Büyük 
+
+
+int main ()
+{
+
+    /*
+    char d = 'J';
+    std::cout << d << std::endl;
+    return 0;
+
+    //	Tek tırnak işaretine dikkat et.(Python'daki gibi çift tırnak kullanımı yok...)
+    //	Watch out for the single quotation mark
+    //	watch out=dikkat et single=tek for the=için
+    */
+
+
+    /*
+    char x[] = "Merhaba";
+    std::cout << x;
+
+    //	Kelime tutmak istiyorsak çift tırnak kullanırız
+    //  Use double quotation marks if we use words
+    //	quotation=tırnak marks=işaretler use=kullanmak words=kelimeler
+    */
+
+
+    /*
+    int x = -10;
+    std::cout << x;
+
+    //	Belirli bir boyutta sabit tutmak istiyorsak farklı int türlerine başvuru yaparız
+    //	If we use a value of a specific size we try a different method
+    //	value=Değer method=yöntem different=farklı specific=özel
+    */
+
+
+    /*
+    double x{ 1231235234.12 };
+    std::cout << std::setprecision(16);
+    std::cout << x;
+
+    //	std::setprecison ayarı hassasiyeti ayarlar. Yani satır sınırlandırmasını biz belirleriz
+    //	we set the line delimitation
+    //	line=hat delimitation=sınırlandırma we set the=ayarlıyoruz setprecison=hassasiyeti ayarla
+    */
+
+
+    /*
+    bool a = false;
+    std::cout << "Bu bizim kac byte alan kapladigini gosterir=> " << sizeof(a) << std::endl; // 1 byte
+    std::cout << "Buda bize direkt a'yi dondurur=> " << a << std::endl;
+    char x[] = "Merhaba";
+    std::cout << sizeof(x) << std::endl; // 8 byte ==> 7 word + 1 empty
+    char y[] = "Merhabalar";
+    std::cout << sizeof(y) << std::endl; // 11 byte ==> 10 word + 1 empty
+    char b[] = "";
+    std::cout << sizeof(b) << std::endl; // 1 byte ==> 0 word + 1 empty*/
+
+
+    /*
+    const int x = 20;
+    x = 10;
+    std::cout << x;
+    // const ile tanımladığımız zaman ifade değiştirilemeyen bir sabite dönüşür. Yani değişken olmaz.
+    // Variables defined with const are constants
+    // Variables=değişkenler defined=Tanımlanmış,tanımlanan const=Sabit
+    */
+
+
+
+    /*
+    float x = 15.13f;
+    std::cout << x << std::endl;
+    std::cout << sizeof(double) << std::endl; // Double bellekte 8 byte'lık yer kaplar.
+    std::cout << sizeof(float) << std::endl;  // Float bellekte 4 byte'lık yer kaplar.
+    std::cout << sizeof(char) << std::endl;   // Char bellekte 1 byte'lık yer kaplar. Önceki dersimizde de görmüştük.
+    std::cout << sizeof(const int);           // const int bellekte 4 byte'lık yer kaplar.
+    */
+
+
+    /*
+    char a = 90;
+    std::cout << static_cast<int>(a);
+    # Normalde int ile tanımlayıp float türünden yazarsak bize hata vericektir.
+    # static_cast<tür>(değişken) sayesinde hata mesajı almadan direkt çevirebiliriz.
+    */
+
+
+    /*
+    char a = 30;
+    std::cout << sizeof(static_cast<int>(a)); // char'ı int'e çevirdik. Bellekte 4 byte yer kapladı.
+    */
+
+
+    /*
+    int a = 80;
+    int x = 21;
+    std::cout << a-- << std::endl; // 80 sonucunu döndürür.
+    std::cout << a << std::endl;   // 79 sonucunu döndürür.
+    std::cout << ++x << std::endl; // 22 sonucunu döndürür.
+    std::cout << x << " " << a << std::endl; // 22 79 sonucunu döndürür. Operatörler ile yapılan işlemler kaydedildi tek kullanımlık değildi...
+    std::cout << x-- << std::endl; // 22 sonucunu döndürür.
+    std::cout << x << std::endl;   // 21 sonucunu döndürür.
+                                   // Sonuç olarak işaretler sonda ise bir kere kendisini verir sonra yapılan işleme göre değişiklik göstererek verir.
+    */
+
+
+    /*
+    OPERATÖRLER
+    int x = 30;
+    int y = 50;
+    // x += y; 80
+    // x -= y; -20 x'ten y'yi çıkardık.
+    // x *= y; 1500
+    // x /= y; 0 x'i y'ye böldük.
+    // x %= 15; 0 x'in 15'e bölümünden kalanı x'e eşitledik.
+    std::cout << x;
+    */
+
+    /*
+    int a = 20; 
+    std::cout << &a << std::endl; // a's position
+    int* aPtr = &a;
+    std::cout << *&*&a << std::endl;
+    std::cout << aPtr << std::endl;  
+    std::cout << *aPtr << std::endl; // 20
+    std::cout << &*aPtr << std::endl;
+    */
+    
+    
+
+
+}
+
+
+
